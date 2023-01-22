7abf9353d916a2d4c65c4de815541c07213aae69
George <georgemarian98@gmail.com>
2023-01-22 14:56:43 +0000

    add scipt to automate docker build and run

---
+++ docker-run-moviebacklog.sh
@@ -0,0 +1,31 @@
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+printf "${GREEN}------------------------------STOP Container------------------------------${NC}\n"
+docker stop moviebacklog
+
+echo $?
+
+printf "${GREEN}------------------------------REMOVE Container------------------------------${NC}\n"
+docker rm moviebacklog
+
+echo $?
+
+printf "${GREEN}------------------------------BUILD Container------------------------------${NC}\n"
+docker build ./MovieBacklog -t movie_backlog_image
+
+if [ ! $? -eq 0 ]
+then
+    printf "${RED}[ERROR] Exit program due to unexpected errors from docker build command...${NC}\n"
+    exit 1
+fi
+
+printf "${GREEN}------------------------------RUN Container------------------------------${NC}\n"
+docker run -d -p 8082:80 --name moviebacklog movie_backlog_image
+
+if [ ! $? -eq 0 ]
+then
+    printf "${RED}[ERROR] Exit program due to unexpected errors from docker run command...${NC}\n"
+    exit 1
+fi
